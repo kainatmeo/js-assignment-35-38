7ebfe43a580a5adeaa2266f2a4c89e90ac7a38ea
kainatmeo <kainatmeo21@gmail.com>
2024-07-05 07:22:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// ___________________________________________Ans 1______________________________________
+// function dateandTime(){
+//     var today=new Date();
+//     document.write(today)
+// }
+
+// dateandTime()
+// ___________________________________________Ans 2______________________________________
+// function Name(){
+//     var firstName=prompt("enter your first name");
+//     var lastname=prompt("enter your last name");
+//     var greets=firstName + lastname
+//     document.write("Asslam o alikum " +greets+"Nice to meet youe");
+// }
+// Name()
+
+// ___________________________________________Ans 3______________________________________
+function User() {
+    var firstNum = +prompt("Enter your first number");
+    var secondNum = +prompt("Enter your second number");
+
+    // Call the sum function
+    sum(firstNum, secondNum);
+
+    // Define the sum function
+    function sum(firstNum, secondNum) {
+        var result = firstNum + secondNum;
+        document.write("The sum is: " + result);
+    }
+}
+
+// Call the User function
+User();
+
+// ___________________________________________Ans 4______________________________________
+
+
+
+
+// ___________________________________________Ans 1______________________________________
+// ___________________________________________Ans 1______________________________________
+// ___________________________________________Ans 1______________________________________
+
